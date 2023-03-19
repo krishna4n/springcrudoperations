7c3c88ec4396848cd1fe45369e99c9c502e8f80e
GK <krishna4n@gmail.com>
2023-03-19 19:00:36 +0530

    Implemented Queries

---
+++ build/resources/main/db/migration/V1_3__Add_field_student.sql
@@ -0,0 +1,2 @@
+ALTER TABLE student
+ADD (mother_name varchar(20), mobile_number int);
---
+++ build/resources/main/db/migration/V1_5__Create_courses.sql
@@ -0,0 +1,5 @@
+CREATE TABLE courses (
+    course_id int NOT NULL AUTO_INCREMENT,
+    course_name varchar(255) NOT NULL,
+    PRIMARY KEY (course_id)
+);
