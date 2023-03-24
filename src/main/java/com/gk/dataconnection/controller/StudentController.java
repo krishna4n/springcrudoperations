b2a8b5d9bfbe8d944d92b231ece76284ab8f3ba8
GK <krishna4n@gmail.com>
2023-03-24 20:35:12 +0530

    Implemented Queries

--- src/main/java/com/gk/dataconnection/controller/StudentController.java
+++ src/main/java/com/gk/dataconnection/controller/StudentController.java
@@ -1,70 +1,42 @@
 package com.gk.dataconnection.controller;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.gk.dataconnection.entity.Courses;
+
 import com.gk.dataconnection.entity.Student;
-import com.gk.dataconnection.repository.*;
+import com.gk.dataconnection.repository.StudentRepository;
+import com.gk.dataconnection.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class StudentController {
-    @Autowired
-    StudentRepository studentRepository;
-    @Autowired
-    CoursesRespository coursesRespository;
-    @Autowired
-    SubjectsRepository subjectsRepository;
 
-    @PostMapping("/save/student")
-    public ResponseEntity<String> saveStudent(@RequestBody  Student student){
 
-        studentRepository.save(student);
-       return ResponseEntity.ok("Saved");
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/student/add")
+    public Student saveStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
     }
+    @DeleteMapping("/student/delete/{student_id}")
+    public String deleteStudent(@PathVariable int student_id){
 
-    @GetMapping("/students")
-    public ResponseEntity<List<Student>> getStudents(){
-        return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
+        return studentService.deleteStudent(student_id);
     }
-
+    @GetMapping("/student")
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
     @GetMapping("/student/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable int id){
-        Optional<Student> student = studentRepository.findById(id);
-        if(student.isPresent()){
-            return new ResponseEntity<>(student.get(), HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return studentService.getStudent(id);
     }
 
-    @PutMapping("/update/student/{id}")
-    public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student stud){
-        Optional<Student> student = studentRepository.findById(id);
-        if(student.isPresent()){
-            student.get().setStudentName(stud.getStudentName());
-            return new ResponseEntity<>(studentRepository.save(student.get()),HttpStatus.OK);
-        }
-        else{
-                return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @PutMapping("/student/{id}")
+    public String updateStudent(@PathVariable int id, @RequestBody Student student){
+        return studentService.updateStudent(id, student);
     }
-
-    @DeleteMapping("/delete/student/{id}")
-    public ResponseEntity<Void> deleteStudent(@PathVariable int id){
-        Optional<Student> student = studentRepository.findById(id);
-        if(student.isPresent()){
-            studentRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
 }
---
+++ src/main/java/com/gk/dataconnection/service/StudentService.java
@@ -0,0 +1,67 @@
+package com.gk.dataconnection.service;
+
+import com.gk.dataconnection.entity.Student;
+import com.gk.dataconnection.repository.CoursesRespository;
+import com.gk.dataconnection.repository.StudentRepository;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+    @Autowired
+    CoursesRespository coursesRespository;
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+
+    }
+
+    public String deleteStudent(int id){
+        Optional<Student> stud =studentRepository.findById(id);
+        if(stud.isPresent()){
+            studentRepository.deleteById(id);
+            return id+" student is deleted successfully";
+        }
+        else {
+            return "No student available with this id"+id;
+        }
+    }
+
+    public ResponseEntity<Student> getStudent(int id){
+        Optional<Student> std = studentRepository.findById(id);
+        if(std.isPresent()){
+            return new ResponseEntity<>(std.get(), HttpStatus.OK) ;
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public String updateStudent(int id,Student student){
+        Optional<Student> st = studentRepository.findById(id);
+        if(st.isPresent()){
+            st.get().setFatherName(student.getFatherName());
+            st.get().setMotherName(student.getMotherName());
+            st.get().setStudentName(student.getStudentName());
+            st.get().setMobileNumber(student.getMobileNumber());
+            studentRepository.save(st.get());
+            return "updated student id "+id;
+        }
+        else{
+            return "Not found id"+id;
+        }
+    }
+
+    public List<Student> getStudents() {
+
+        return studentRepository.findAll();
+    }
+}
